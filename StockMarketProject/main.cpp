41e6fde3bdeb51bcca1f648e75343ab94d91fb0d
Your Name <you@example.com>
2020-11-28 20:54:01 +0100

    Added project, ya'll

---
+++ StockMarketProject/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "_______________________________________" << std::endl;
+    std::cout << "     Welcome To Northern Networks    " << std::endl;
+    std::cout << "  Your Modern Stock Market Platform   " << std::endl;
+    std::cout << "_______________________________________" << std::endl;
+
+    return 0;
+}
